70ba63b0600e425a0987c41a2effa536e896da12
RM550948 <rm550948@fiap.com.BR>
2025-03-09 16:37:11 -0300

    Fix: Create api flask_restx

--- routes/app.py
+++ routes/app.py
@@ -1,5 +1,11 @@
 from flask import Flask
-from flask_restx import Api
+from flask_restx import Api, Resource
+
 
 app = Flask(__name__)
 api = Api(app)
+
+@api.route('/get_results')
+class GetExcel(Resource):
+    def get(self):
+        return True
